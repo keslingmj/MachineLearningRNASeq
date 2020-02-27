3d56792579dd5274aa91154dbb7d998200d8fbf3
keslingmj <mk@michaelkesling.com>
2020-02-26 19:20:13 -0600

    python code update

--- Toil_Analysis_ObjOrient.py
+++ Toil_Analysis_ObjOrient.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/Users/mjk/opt/anaconda3/bin/python3
+#### #!/Users/mjk/.virtualenvs/ML/bin/python3
 # -*- coding: utf-8 -*-
 """
 Toil_Analysis_ObjOrient.py
@@ -31,8 +32,11 @@ import re
 import numpy as np
 import matplotlib as plt
 import random
+import rpy2.robjects as robjects
 from sklearn.model_selection import train_test_split
-
+from rpy2.robjects.packages import importr
+from itertools import compress
+import copy
 
 
 
@@ -67,7 +71,7 @@ def geneNameMod(toilSubset, TOILGENEANNOT):
     # this code merges gene ID with common gene name,
     # and otherwise returns the toilSubset dataframe unaltered
     
-    toilGeneAnnot = pd.read_csv(TOILGENEANNOT, sep="\t")
+    toilGeneAnnot = pd.read_csv(TOILGENEANNOT, sep="\t", engine="python")
     id2gene = dict(zip(toilGeneAnnot.loc[:,'id'], toilGeneAnnot.loc[:,'gene']))
     toilSubset['gene']=[id2gene[x] for x in toilSubset.index]  # list comp
     
@@ -80,27 +84,74 @@ def geneNameMod(toilSubset, TOILGENEANNOT):
 
 # pickRefSample
 # filterGenes
+    
+## NOT FINISHED
+def filterGenes(x, cutoff=0.2):    # assumes data in log2-format
+    return(x)
+    
 # addZeroGenes
 
 
 # 4. Also need to create a structure for holding various dataframes.
+class RSeqDF(pd.DataFrame):
+    def __init__(self, name, data=None, index=None, columns=None,
+                 dtype=None, copy=False):
+        pd.DataFrame.__init__(self, data=data, index=index, columns=columns,
+                 dtype=dtype, copy=copy)
+        self.name = name
+        
+    def __len__(self):              # returns number of rows in pd.DF
+        return self.shape[0]
+    
+    def __getitem__(self):
+        return self
+
+class DF_Set:
+    def __init__(self, name, data=None, index=None, columns=None,
+                 dtype=None, copy=False):
+        objNames = "orig filt zeroGenes nat scaled norm outcome refSmpName \
+        refSmp refSmpUnscaled filtScaled".split()
+        self._dfs = [RSeqDF('orig', data=data, index=index, \
+                 columns=columns, dtype=None, copy=copy)]
+        self.nameIdx = dict(zip(objNames, range(11)))
+        
+    def __getitem__(self, name):                    # looks up DF based on name
+        return self._dfs[self.nameIdx[name]]             # name -> posn in list
+    
+    def filterGenes(self, cutoff=0.2):                     # assumes log2 scale
+        ZeroExpGenes = list(compress(self._dfs[0], \
+                                     self._dfs[0].quantile(.75, axis=0) < cutoff))
+        orig = copy.copy(self._dfs[0])
+        self.filt = orig.drop(ZeroExpGenes, axis=1, inplace=False)
+        
+        self._dfs.append(self.filt)
+        self._dfs.append(ZeroExpGenes)
+        #return self.filt
+    
+    def natScale(self):                         # full orig df on natural scale
+        self._dfs.append(np.exp2(self._dfs[0]) - 1)
+        
+        
+        
+    #def name2Idx(self, name):
+    #    return self.nameIdx[name]
+
 
 ##########
 # MAIN
 ##########
 
-# Read in main estimated-counts file
+# 1. Read in main estimated-counts file
 toilSubset = pd.read_csv(RSEM_COUNTS_FILE, sep="\t")
 toilSubset.rename(index = dict(zip(toilSubset.index, toilSubset.loc[:,'sample'])),
                   inplace=True)
 toilSubset.drop(['sample'], axis=1, inplace=True)
 
-# gene name modification
+# 2. gene name modification and sort samples
 toilSubset = geneNameMod(toilSubset, TOILGENEANNOT)
-# sort samples
 toilSubset = processColReorder(toilSubset)
 
-# Create Training and Test Sets from toilSubset
+# 3. Create Training and Test Sets from toilSubset
 # A. transpose the matrix
 toilSubsetWide = toilSubset.T
 
@@ -130,4 +181,21 @@ outcomeTest = outcomeTest.loc[toilTest.index,:]
 all(outcomeTrain.index == toilTrain.index)
 all(outcomeTest.index == toilTest.index)
 sum(outcomeTrain[0])/len(outcomeTrain.index) # tests even split healthy/tumor
+
+# 4. Create Data Object
+# FIRST, GET TO WORK.  THEN CHECK ACCURACY, THEN, WILL WRITE UP PROPERLY
+train_orig = RSeqDF('orig', data=toilTrain, index=toilTrain.index, columns=toilTrain.columns)
+train_norm = train_orig.transform(lambda x: 2**x - 1)
+geneQuantiles = train_orig.quantile(q=0.75, axis='rows') # counter-intuitive
+ZeroGenes = geneQuantiles < 0.2
+train_filt = train_orig.loc[:, -ZeroGenes]
+#edgeR_scalingFactors = edgeR::calcNormFactors(t(train$M_filt),method="TMM")
+
+
+# 5. Filter Out Zero-Expressed Genes
+#r = robjects.r
+limma = importr('limma')
+edgeR = importr('edgeR')
+robjects.r("library(edgeR)")   # working now.
+robjects.r("version")
 
